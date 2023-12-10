41b4d575cc796098f19e1af8e4e0a232d7056532
LucasFishwicas <luke-fishwick_@hotmail.com>
2023-12-09 21:11:11 -0500

    Completed the tutorial for creating Conways Game of Life using OpenGL in Go Written by @kylewbanks on Mar 12 2017  - kylewbanks.com

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+    "time"
+	"fmt"
 	"log"
-    "fmt"
-    "strings"
+	"math/rand"
 	"runtime"
+	"strings"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
@@ -14,8 +16,10 @@ import (
 const (
 	width  = 500
 	height = 500
-    rows = 10
-    columns = 10
+    rows = 100
+    columns = 100
+    threshold = 0.15
+    //fps = 10
     
     // code to be run in the GPU Driver
     vertexShaderSource = `
@@ -55,6 +59,9 @@ var (
 type cell struct {
     drawable uint32
 
+    alive bool
+    aliveNext bool
+
     x int
     y int
 }
@@ -62,10 +69,80 @@ type cell struct {
 //GO_NOTE// Pointer in the brackets refers to the object the method is for
 // c.draw() to be used within the draw() function
 func (c *cell) draw() {
+    if !c.alive {
+        return
+    }
+
     gl.BindVertexArray(c.drawable)
     gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square) / 3))
 }
 
+// checkState determines the state of the cell for the next tick of the game.
+func (c *cell) checkState(cells [][]*cell) {
+    c.alive = c.aliveNext
+    c.aliveNext = c.alive
+
+    liveCount := c.liveNeighbours(cells)
+    if c.alive {
+        // 1. Any live cell with fewer than two live neighbours dies, as if
+        // caused by underpopulation.
+        if liveCount < 2 {
+            c.aliveNext = false
+        }
+
+        // 2. Any live cell with two or three live neighbours lives on to the
+        // next generation.
+        if liveCount == 2 || liveCount == 3 {
+            c.aliveNext = true
+        }
+
+        // 3. Any live cell with more than three live neighbours dies, as if by
+        // overpopulation.
+        if liveCount > 3 {
+            c.aliveNext = false
+        }
+    } else {
+        // 4. Any dead cell with exactly three live neighbours becomes a live
+        // cell, as if by reproduction.
+        if liveCount == 3 {
+            c.aliveNext = true
+        }
+    }
+}
+
+// liveNeighbours returns the number of live neighbours for a cell.
+func (c *cell) liveNeighbours(cells [][]*cell) int {
+    var liveCount int
+    add := func(x, y int) {
+        // If we're at an edge, check the other side of the board.
+        if x == len(cells) {
+            x = 0
+        } else if x == -1 {
+            x = len(cells) - 1
+        }
+
+        if y == len(cells[x]) {
+            y = 0
+        } else if y == -1 {
+            y = len(cells[x]) - 1
+        }
+
+        if cells[x][y].alive {
+            liveCount++
+        }
+    }
+
+    add(c.x-1 ,c.y)     // To the left
+    add(c.x+1 ,c.y)     // To the right
+    add(c.x ,c.y+1)     // Up
+    add(c.x ,c.y-1)     // Down
+    add(c.x-1 ,c.y+1)   // Top-left
+    add(c.x+1 ,c.y+1)   // Top-right
+    add(c.x-1 ,c.y-1)   // Bottom-left
+    add(c.x+1 ,c.y-1)   // Bottom-right
+
+    return liveCount
+}
 
 
 
@@ -81,9 +158,20 @@ func main() {
 	//vao := makeVao(square)
     cells := makeCells()
 
-	for !window.ShouldClose() {
-		//TODO
-		draw(cells, window, program)
+
+    // Loop terminates when the window is closed
+	for !window.ShouldClose() { 
+     //   t := time.Now()
+
+        for x := range cells {
+            for _, c := range cells[x] {
+                c.checkState(cells)
+            }
+        }
+
+        draw(cells, window, program)        
+
+       // time.Sleep(time.Second/time.Duration(fps) - time.Since(t))
 	}
 }
 
@@ -95,10 +183,16 @@ func main() {
 
 // makeCells creates a multi-dimensional slice to represent the board
 func makeCells () [][]*cell {
+    rand.Seed(time.Now().UnixNano())
+
     cells := make([][]*cell, rows, rows)
     for x := 0; x < rows; x++ {
         for y := 0; y < columns; y++ {
             c := newCell(x, y)
+
+            c.alive = rand.Float64() < threshold
+            c.aliveNext = c.alive
+
             cells[x] = append(cells[x], c)
         }
     }
